276c8d0d0d5da6303c04fe93dbb2848f4c04f948
karthikraman30 <144605149+karthikraman30@users.noreply.github.com>
2024-07-04 17:14:08 +0530

    Create two_sum_II.cpp

---
+++ two_sum_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int a = 0;
+        int b = numbers.size()-1;
+        vector<int> res{};
+        while(a<b){
+            if(numbers[a]+numbers[b] == target){
+                res.push_back(a+1);
+                res.push_back(b+1);
+                break;
+            }else if(numbers[a]+numbers[b] < target){
+                a++;
+            }else{
+                b--;
+            }
+        }
+        return res;
+    }
+};
