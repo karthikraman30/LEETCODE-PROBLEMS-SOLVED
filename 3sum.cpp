215de1669da12e2df758d95a4c864ae7a71b1894
karthik raman <karthikraman@Balamurugans-MacBook-Pro.local>
2024-07-04 19:26:19 +0530

    feat: updated code for 3sum problem

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,67 +1,74 @@
-#include <iostream>
-#include<vector>
-
-using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        cout<<"Started";
-        for(auto i:nums){
-            cout << i << ',';
-        }
-        cout <<endl;
         vector<vector<int>> result;
-        int start = 0, end = nums.size() - 1, mid = (start + end) / 2;
-        cout <<" Calculated S,M,E" << start<<", " << mid<<", " << end << endl;
-        int iter =0 ;
-        while (start < end) {
-            iter ++;
-            int compute = nums[start] + nums[mid] + nums[end];
-            cout << "Try : "<< iter << " with index : " << start<<", " << mid<<", " << end << endl;
-            cout << "Compute : "<< compute << endl;
-            if (compute == 0) {
-                vector<int> temp_arr;
-                temp_arr.push_back(nums[start]);
-                temp_arr.push_back(nums[mid]);
-                temp_arr.push_back(nums[end]);
-                result.push_back(temp_arr);
-                start++;
-            } else if (compute < 0) {
-                start++;
-                if (start == mid) {
-                    mid++;
+        sort(nums.begin(), nums.end());
+        int size = nums.size();
+        for (int i = 0; i < size - 2; i++) {
+            // Skip duplicates for i
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            
+            int low = i + 1, high = size - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[low], nums[high]});
+                    
+                    // Skip duplicates for low and high
+                    while (low < high && nums[low] == nums[low+1]) low++;
+                    while (low < high && nums[high] == nums[high-1]) high--;
+                    
+                    low++;
+                    high--;
                 }
-                if (mid == end) {
-                    cout << "Moved : "<< iter << " with index : " << start<<", " << mid<<", " << end << endl;
-                    break;
+                else if (sum < 0) {
+                    low++;
                 }
-            } else {
-                end--;
-                if (end == mid) {
-                    mid--;
-                }
-                if (start == mid) {
-                    cout << "Moved : "<< iter << " with index : " << start<<", " << mid<<", " << end << endl;
-                    break;
+                else {
+                    high--;
                 }
             }
-            cout << "Moved : "<< iter << " with index : " << start<<", " << mid<<", " << end << endl;
+        }
+        return result;
+    }
+};
+/*class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        int size = nums.size();
+        for(int i=0;i<size;i++){
+            int low = i+1,high = size-1;
+            while(low<high){
+                int sum = nums[i]+nums[low]+nums[high];
+                if(sum == 0){
+                    vector<int> temp_arr;
+                    temp_arr.push_back(nums[i]);
+                    temp_arr.push_back(nums[low]);
+                    temp_arr.push_back(nums[high]);
+                    result.push_back(temp_arr);
+                }
+                else if(sum < 0){
+                    low++;
+                }
+                else{
+                    high--;
+                }
+            }
         }
         return result;
     }
 };
 
-int main(){
-    Solution s;
-    vector<int> test_case = {-1,0,1,2,-1,-4};
-    vector<vector<int>> result = s.threeSum(test_case);
-    for( auto value: result){
-        cout << "Case : ";
-        for(auto index:value){
-            cout << index << ",";
-        }
-        cout <<endl;
-    }
-    return 0;
-}
 
+1. sort
+ex: nums = [-1,0,1,2,-1,-4]
+3
+0
+-4.    -1.    -1.          0.          1.     2.
+                                       START.   MIDEND
+
+START + MID + END -> 0 + 1 + 2
+
+*/
