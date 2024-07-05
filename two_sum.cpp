53bd6a608e44383d8ae8a0828c2b32cd54efcc1d
karthik raman <karthikraman@Balamurugans-MacBook-Pro.local>
2024-07-05 13:21:12 +0530

    two sum program is added

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,1 +1,31 @@
-
+#include<iostream>
+#include<vector>
+#include<list>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, list<int>> positions;
+        for (int i = 0; i < nums.size(); i++) {
+            positions[nums[i]].push_back(i);   
+        }
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        int right = nums.size() - 1;
+        vector<int> result;
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                result.push_back(positions[nums[left]].front());
+                positions[nums[left]].pop_front();
+                result.push_back(positions[nums[right]].front());
+                positions[nums[right]].pop_front();
+                break;
+            } else if (nums[left] + nums[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+};
